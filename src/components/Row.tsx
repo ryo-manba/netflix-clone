1f473ae34369936c6d2f7add8abb0d4348b575e6
Ryo <matsukawa.ryo42@gmail.com>
2022-09-28 23:19:20 +0900

    add: Click on the movie image to view the trailer

--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "../axios";
 import "./Row.scss";
+import YouTube from "react-youtube";
 
 const base_url = "https://image.tmdb.org/t/p/original";
 
@@ -19,8 +20,17 @@ type Movie = {
   backdrop_path: string;
 };
 
+type Options = {
+  height: string;
+  width: string;
+  playerVars: {
+    autoplay: 0 | 1 | undefined;
+  };
+};
+
 export const Row = ({ title, fetchUrl, isLargeRow }: Props) => {
   const [movies, setMovies] = useState<Movie[]>([]);
+  const [trailerUrl, setTrailerUrl] = useState<string | null>("");
 
   useEffect(() => {
     async function fetchData() {
@@ -31,7 +41,26 @@ export const Row = ({ title, fetchUrl, isLargeRow }: Props) => {
     fetchData();
   }, [fetchUrl]);
 
-  console.log(movies);
+  const opts: Options = {
+    height: "390",
+    width: "640",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = async (movie: Movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      let trailerUrl = await axios.get(
+        `/movie/${movie.id}/videos?api_key=${process.env.REACT_APP_API_KEY}`
+      );
+      setTrailerUrl(trailerUrl.data.results[0]?.key);
+    }
+  };
+
   return (
     <div className="Row">
       <h2>{title}</h2>
@@ -44,9 +73,11 @@ export const Row = ({ title, fetchUrl, isLargeRow }: Props) => {
               isLargeRow ? movie.poster_path : movie.backdrop_path
             }`}
             alt={movie.name}
+            onClick={() => handleClick(movie)}
           />
         ))}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 };
--- src/request.ts
+++ src/request.ts
@@ -2,7 +2,7 @@ const API_KEY = process.env.REACT_APP_API_KEY
 
 export const requests = {
   fetchTrending:`/trending/all/week?api_key=${API_KEY}&language=en-us`,
-  fetchNetflixOriginals:`/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchNetflixOriginals:`/discover/movie?api_key=${API_KEY}&with_networks=213`,
   fetchTopRated:`/discover/tv?api_key=${API_KEY}&languager=en-us`,
   fetchActionMovies:`/discover/tv?api_key=${API_KEY}&with_genres=28`,
   fetchComedyMovies:`/discover/tv?api_key=${API_KEY}&with_genres=35`,
